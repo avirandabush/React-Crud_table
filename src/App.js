9c6eb2e59aa80fa561d8a834681effc995ff2ff3
aviran dabush <avirandabush@gmail.com>
2022-03-22 15:30:44 +0200

    pattern and input initializing

--- src/App.js
+++ src/App.js
@@ -6,149 +6,153 @@ import EditRows from './componenets/EditRows';
 
 function App() 
 {
-//store the data in the state
-const [users, setUsers] = useState(data);
+  //store the data in the state
+  const [users, setUsers] = useState(data);
 
-const initialValue ={
-  id: '',
-  firstName: '',
-  lastName: ''
-}
-//get data from 'add user' form
-const [addFormData, setAddFormData] = useState(initialValue)
-
-//set message for success or failure
-const [addingMessage, setAddingMessage] = useState(`I'm waiting...`);
-
-//edit user input data
-const [editFormData, setEditFormData] = useState({
-  id: '',
-  firstName: '',
-  lastName: ''
-})
-
-//edit user
-const [editUserId, setEditUserId] = useState(null);
-
-//manage the input data by the state
-const handleAddFormChange = (event) => 
-{
-  event.preventDefault();
-
-  //get the name attribute from the inputs
-  const fieldName = event.target.getAttribute('name');
-  //get the values
-  const fieldValue = event.target.value;
-  //make copy of the existing form data
-  const newFormData = {...addFormData};
-  //update the data with the new value
-  newFormData[fieldName] = fieldValue;
-  //set into state, call the set function
-  setAddFormData(newFormData);
-}
-
-//manage editing input data by the state
-const handleEditFormChange = (event) => 
-{
-  event.preventDefault();
-
-  //get the name attribute from the inputs
-  const fielsName = event.target.getAttribute('name');
-  //get the values
-  const fieldValue = event.target.value;
-  //make copy of the existing form data
-  const newFormData = {...editFormData};
-  //update the data with the new value
-  newFormData[fielsName] = fieldValue;
-  //set into state, call the set function
-  setEditFormData(newFormData);
-}
-
-//handle the new user adding when submit
-const handleAddFormSubmit = (event) =>
-{
-  event.preventDefault();
-
-  //take the input data and make a new object
-  const newUser = 
+  //initiate values seperatly, to clear input field after submit
+  const initialValue =
   {
-    id: addFormData.id,
-    firstName: addFormData.firstName,
-    lastName: addFormData.lastName
+    id: '',
+    firstName: '',
+    lastName: ''
   }
-  //check for duplicated id
-  if(users.find(users => users.id === newUser.id))
+
+  //get data from 'add user' form
+  const [addFormData, setAddFormData] = useState(initialValue)
+
+  //set message for success or failure
+  const [addingMessage, setAddingMessage] = useState(`I'm waiting...`);
+
+  //edit user input data
+  const [editFormData, setEditFormData] = useState({
+    id: '',
+    firstName: '',
+    lastName: ''
+  })
+
+  //edit user
+  const [editUserId, setEditUserId] = useState(null);
+
+  //manage the input data by the state
+  const handleAddFormChange = (event) => 
   {
-    setAddingMessage('ID is alredy exsist');
-    return;
+    event.preventDefault();
+
+    //get the name attribute from the inputs
+    const fieldName = event.target.getAttribute('name');
+    //get the values
+    const fieldValue = event.target.value;
+    //make copy of the existing form data
+    const newFormData = {...addFormData};
+    //update the data with the new value
+    newFormData[fieldName] = fieldValue;
+    //set into state, call the set function
+    setAddFormData(newFormData);
   }
-  //create a new users array
-  const newUsers = [...users, newUser];
-  //call the function and set a new array
-  setUsers(newUsers);
-  setAddingMessage('User added successfully!');
-  setAddFormData(initialValue);
-}
 
-//handle the edited user adding when submit
-const handleEditedFormSubmit = (event) =>
-{
-  event.preventDefault();
-
-  //take the input data and make a new object
-  const editedUser = 
+  //manage editing input data by the state
+  const handleEditFormChange = (event) => 
   {
-    id: editUserId,
-    firstName:editFormData.firstName,
-    lastName: editFormData.lastName
-  }
-  
-  //create a new users array
-  const newUsers = [...users];
-  //get the index of the editing row
-  const index = users.findIndex((user) => user.id === editUserId);
-  //update the array in the correct position
-  newUsers[index] = editedUser;
-  //call the function and set a new array
-  setUsers(newUsers);
-  setEditUserId(null);
-}
+    event.preventDefault();
 
-//handle the edit button
-const handleEditClick = (event, user) =>
-{
-  event.preventDefault();
-  //set the edit
-  setEditUserId(user.id);
-  //make edit object
-  const formValues = 
+    //get the name attribute from the inputs
+    const fielsName = event.target.getAttribute('name');
+    //get the values
+    const fieldValue = event.target.value;
+    //make copy of the existing form data
+    const newFormData = {...editFormData};
+    //update the data with the new value
+    newFormData[fielsName] = fieldValue;
+    //set into state, call the set function
+    setEditFormData(newFormData);
+  }
+
+  //handle the new user adding when submit
+  const handleAddFormSubmit = (event) =>
   {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName
+    event.preventDefault();
+
+    //take the input data and make a new object
+    const newUser = 
+    {
+      id: addFormData.id,
+      firstName: addFormData.firstName,
+      lastName: addFormData.lastName
+    }
+    //check for duplicated id
+    if(users.find(users => users.id === newUser.id))
+    {
+      setAddingMessage('ID is alredy exsist');
+      return;
+    }
+    //create a new users array
+    const newUsers = [...users, newUser];
+    //call the function and set a new array
+    setUsers(newUsers);
+    setAddingMessage('User added successfully!');
+    //clear input fiels after submit
+    setAddFormData(initialValue);
   }
-  //set edited data
-  setEditFormData(formValues);
-}
 
-//make an cancel option for editing
-const handleCancelClick = () => 
-{
-  setEditUserId(null);
-}
+  //handle the edited user adding when submit
+  const handleEditedFormSubmit = (event) =>
+  {
+    event.preventDefault();
 
-//handle the delete user option
-const handleDeleteClick = (userId) =>
-{
-  //create new array
-  const newUsers = [...users];
-  //find the index you want to delete
-  const index = users.findIndex((user) => user.id === userId)
-  //remove the chosen row
-  newUsers.splice(index, 1);
-  //save changes into state
-  setUsers(newUsers);
-}
+    //take the input data and make a new object
+    const editedUser = 
+    {
+      id: editUserId,
+      firstName:editFormData.firstName,
+      lastName: editFormData.lastName
+    }
+    
+    //create a new users array
+    const newUsers = [...users];
+    //get the index of the editing row
+    const index = users.findIndex((user) => user.id === editUserId);
+    //update the array in the correct position
+    newUsers[index] = editedUser;
+    //call the function and set a new array
+    setUsers(newUsers);
+    setEditUserId(null);
+  }
+
+  //handle the edit button
+  const handleEditClick = (event, user) =>
+  {
+    event.preventDefault();
+    //set the edit
+    setEditUserId(user.id);
+    //make edit object
+    const formValues = 
+    {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName
+    }
+    //set edited data
+    setEditFormData(formValues);
+  }
+
+  //make an cancel option for editing
+  const handleCancelClick = () => 
+  {
+    setEditUserId(null);
+  }
+
+  //handle the delete user option
+  const handleDeleteClick = (userId) =>
+  {
+    //create new array
+    const newUsers = [...users];
+    //find the index you want to delete
+    const index = users.findIndex((user) => user.id === userId)
+    //remove the chosen row
+    newUsers.splice(index, 1);
+    //save changes into state
+    setUsers(newUsers);
+  }
 
   return (
     <div className="app-container">
